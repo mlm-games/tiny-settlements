e5d13e0167e9a837b19adab7746ac164527286ce
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-09-14 11:17:43 +0530

    chore: pin

--- src/app.rs
+++ src/app.rs
@@ -381,6 +381,7 @@ impl Plugin for AppPlugin {
                     compose_every_frame: true,
                     msaa_samples: 1,
                     overlay: true,
+                    ..Default::default()
                 },
                 move |_s, _c| {
                     let st = shared_ui.lock().unwrap().clone();
